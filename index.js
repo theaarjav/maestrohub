b1ad20f2944efbaa33e190b29d0cbf090591be9a
theaarjav <aarjavdesai22@gmail.com>
2023-07-17 13:02:39 +0530

    demodified index.js

--- index.js
+++ index.js
@@ -7,13 +7,8 @@ const bodyParser=require("body-parser");
 
 const mongoose=require("mongoose");
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,   
-            //  access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(bodyParser.json({limit:"50mb"}));
 
