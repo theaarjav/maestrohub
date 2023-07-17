7166b972cef601120bc79e0784ae530478b98c50
theaarjav <aarjavdesai22@gmail.com>
2023-07-17 12:51:02 +0530

    added cors origin

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const bodyParser=require("body-parser");
 
 const mongoose=require("mongoose");
 
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,   
+            //  access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json({limit:"50mb"}));
 
