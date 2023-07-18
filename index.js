f601ae935b17efee4d7eda872388c7e31a15da92
theaarjav <aarjavdesai22@gmail.com>
2023-07-18 13:00:19 +0530

    errors

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const mongoose=require("mongoose");
 
 
 app.use(cors());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(bodyParser.json({limit:"50mb"}));
 
 app.use(bodyParser.urlencoded({limit:"50mb", parameterLimit:100000, extended:true}));
