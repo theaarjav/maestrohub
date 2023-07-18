a2838b128d98663ec7e2e6aa30e97c9441911444
theaarjav <aarjavdesai22@gmail.com>
2023-07-18 17:13:23 +0530

    removed unnecessary social json

--- models/Profile.js
+++ models/Profile.js
@@ -1,66 +1,65 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const ProfileSchema= new mongoose.Schema({
-    user:{
+const ProfileSchema = new mongoose.Schema({
+    user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref:"user",
-        required:false
+        ref: "user",
+        required: false
     },
-    
+
     // company:{type:String},
-    squat:{type:String},
-    bench:{type:String},
-    deadlift:{type:String},
-    location:{
-        
-            type:String
-        
+    squat: { type: String },
+    bench: { type: String },
+    deadlift: { type: String },
+    location: {
+
+        type: String
+
 
     },
-    
-    skills:{
-        type:[String], required:false
+
+    skills: {
+        type: [String], required: false
     },
-    bio:{
+    bio: {
         type: String,
-        required:false
+        required: false
     },
-    trainer:{
-        type:Boolean
+    trainer: {
+        type: Boolean
     },
-    competitions:[{
-        title:{
-            type:String
+    competitions: [{
+        title: {
+            type: String
         },
-        represented:{type:String},
-        squat:{type:String},
-        bench:{type:String},
-        deadlift:{type:String},
-        snatch:{type:String},
-        cnj:{type:String},
-        position:{type:String},
-        location:{type:String},
-        from:{type:String},
-        to:{type:String},
-        description:{type:String}
+        represented: { type: String },
+        squat: { type: String },
+        bench: { type: String },
+        deadlift: { type: String },
+        snatch: { type: String },
+        cnj: { type: String },
+        position: { type: String },
+        location: { type: String },
+        from: { type: String },
+        to: { type: String },
+        description: { type: String }
     }
     ]
     ,
-    
-    social:
-        {
-            youtube: { type: String, required: false },
-            facebook: { type: String, required: false },
-            twitter: { type: String, required: false },
-            linkedin: { type: String, required: false },
-            instagram: { type: String, required: false },
-        },
-    
-    followers:[{type: mongoose.Schema.Types.ObjectId, ref:"user"}],
-    following:[{type: mongoose.Schema.Types.ObjectId, ref:"user"}],
-    },
-    {timestamps:true}
+
+
+    youtube: { type: String, required: false },
+    facebook: { type: String, required: false },
+    twitter: { type: String, required: false },
+    linkedin: { type: String, required: false },
+    instagram: { type: String, required: false },
+
+
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+},
+    { timestamps: true }
 )
 
-const Profile=mongoose.model("profile", ProfileSchema);
-module.exports=Profile;
+const Profile = mongoose.model("profile", ProfileSchema);
+module.exports = Profile;
